8fa38cc1de5397fbe31b1dfb15023359a12cbaeb
YangSiming <1608839567@qq.com>
2023-05-31 12:59:01 +0800

    实现mybatis增删改查

--- mybatis-study/src/main/java/com/ysm/mybatis/mapper/UserMapper.java
+++ mybatis-study/src/main/java/com/ysm/mybatis/mapper/UserMapper.java
@@ -1,5 +1,15 @@
 package com.ysm.mybatis.mapper;
 
+import com.ysm.mybatis.pojo.User;
+
+import java.util.List;
+
 public interface UserMapper {
     int insertUser();
+
+    int updateUser();
+    int deleteUser();
+    User getUserById();
+
+    List<User> listAllUser();
 }
--- mybatis-study/src/test/java/MybatisTest.java
+++ mybatis-study/src/test/java/MybatisTest.java
@@ -1,4 +1,6 @@
 import com.ysm.mybatis.mapper.UserMapper;
+import com.ysm.mybatis.pojo.User;
+import com.ysm.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -7,6 +9,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MybatisTest {
 
@@ -17,12 +20,45 @@ public class MybatisTest {
         //获取
         SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
         SqlSessionFactory sessionFactory = sqlSessionFactoryBuilder.build(stream);
-        SqlSession sqlSession = sessionFactory.openSession();
+        //自动提交事务
+        SqlSession sqlSession = sessionFactory.openSession(true);
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         System.out.println(userMapper.insertUser());
         sqlSession.commit();
         sqlSession.close();
+    }
 
+    @Test
+    public void testUpdate(){
+        final SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        System.out.println(userMapper.updateUser());
+        sqlSession.close();
+    }
+    @Test
+    public void testDelete(){
+        final SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        System.out.println(userMapper.deleteUser());
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSelect(){
+        final SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        final User user = userMapper.getUserById();
+        System.out.println(user);
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSelectAllUser(){
+        final SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        final List<User> users = userMapper.listAllUser();
+        users.forEach(System.out::println);
+        sqlSession.close();
     }
 
 }
